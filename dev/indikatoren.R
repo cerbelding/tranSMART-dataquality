9206032060ae202ab0b945e8c7b8386f87bbe5b7
Cornelius <crashfighter@users.noreply.github.com>
2019-12-08 11:44:26 +0100

    added TMF-1007

--- dev/indikatoren.R
+++ dev/indikatoren.R
@@ -41,6 +41,7 @@ tmf1017_names <- rownames(tmp)
 rm(tmp)
 
 # entferne alle Variablen aus 'input_data', zu denen ueberhaupt kein Eintrag besteht
+# setzt alle EIntraege zu Text statt numeric
 library(data.table)
 t_input_data <- transpose(input_data)
 colnames(t_input_data) <- rownames(input_data)
@@ -51,3 +52,22 @@ colnames(input_data_cleaned) <- rownames(t_input_data_cleaned)
 rownames(input_data_cleaned) <- colnames(t_input_data_cleaned)
 rm(t_input_data,t_input_data_cleaned)
 
+#TMF-1007 - Bevorzugung bestimmter Endziffern
+#betrifft die Variablen age_years, bmi, weight_kg und height_cm
+#nur bedingt aussagekraeftig, da gemessen
+tmp.nums <- unlist(lapply(input_data, is.numeric))
+input_data_numeric <- input_data[, tmp.nums]
+library(stringr)
+tmf1007 <- NULL
+
+for (i in 1:NROW(input_data_numeric)) {
+  tmf1007$age_years[i]  <- str_sub(input_data_numeric$age_years[i], start = -1)
+  tmf1007$bmi[i]  <- str_sub(floor(input_data_numeric$bmi[i]), start = -1) #abrunden
+  tmf1007$height_cm[i]  <- str_sub(input_data_numeric$height_cm[i], start = -1)
+  tmf1007$weight_kg[i]  <- str_sub(input_data_numeric$weight_kg[i], start = -1)
+}
+table(tmf1007$age_years)
+table(tmf1007$bmi)
+table(tmf1007$bmi)
+table(tmf1007$height_cm)
+table(tmf1007$weight_kg)
