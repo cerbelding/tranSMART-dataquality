d72991b6d8793a82d9a5118ca20384a3383bb300
Cornelius Knopp <corneliusknopp@users.noreply.github.com>
2020-01-14 12:19:09 +0100

    modified Visualization

--- SmartR/web-app/js/smartR/_angular/viz/d3Dataquality.js
+++ SmartR/web-app/js/smartR/_angular/viz/d3Dataquality.js
@@ -11,56 +11,26 @@ window.smartRApp.directive('dataquality', [
         return {
             restrict: 'E',
             scope: {
-                data: '=',
-                width: '@',
-                height: '@'
+                data: '='
             },
             templateUrl: $rootScope.smartRPath +  '/js/smartR/_angular/templates/dataquality.html',
             link: function (scope, element) {
-                var template_ctrl = element.children()[0],
-                    template_viz = element.children()[1];
+                var vizDiv = element.children()[0];
                 /**
                  * Watch data model (which is only changed by ajax calls when we want to (re)draw everything)
                  */
                 scope.$watch('data', function () {
-                    $(template_viz).empty();
+                    $(vizDiv).empty();
                     if (! $.isEmptyObject(scope.data)) {
-                        smartRUtils.prepareWindowSize(scope.width, scope.height);
-                        scope.showControls = true;
-                        createPedigree(scope, template_viz, template_ctrl);
+                        reportDataquality(scope, vizDiv);
                     }
                 });
             }
         };
 
 
-        function createPedigree(scope, vizDiv) {
-
-        // <img style="height=10%" src="/tmp/Rserv/conn22738/Bild1.png"
-        // class="img-result-size_sysinflame"/>
-            var width = parseInt(scope.width);
-            var height = parseInt(scope.height);
-            var margin = {top: 20, right: 60, bottom: 200, left: 280};
-
-            var svg = d3.select(vizDiv).append("svg").attr("width",
-                width + margin.left + margin.right).attr("height",
-                height + margin.top + margin.bottom).append("g").attr(
-                "transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-
-svg.append('img')
-    .attr('class', 'picture')
-    .attr('src', "data:image/png;base64," + scope.data.plots[0]);
-
-
-            for (var i = 0; i < scope.data.plots.length ; i++) {
-                document.getElementById("ped").src = "data:image/png;base64," + scope.data.plots[0];
-            }
-            console.log(scope)
-            console.log(scope.data);
-
+        function reportDataquality(scope, vizDiv) {
+        	document.write("Dataquality Report:");
         }
-
-
     }]);
-
+        
