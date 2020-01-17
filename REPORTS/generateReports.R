247cacad3418c41bc39509f220b2848f6f6d481e
Cornelius Knopp <corneliusknopp@users.noreply.github.com>
2020-01-17 18:50:08 +0100

    updated generateReports.R

--- REPORTS/generateReports.R
+++ REPORTS/generateReports.R
@@ -1,6 +1,8 @@
 ## Script to generate Reports
 
-# Step 0: Load data
+# Step 0: Load Libraries and  data
+library(dlookr)
+library(dataQualityR)
 input_data <- read.delim("clinical/subjects_data.tsv", na.strings="N/A")
 
 # Step 1: Diagnose_Report
@@ -11,3 +13,10 @@ diagnose_report(input_data, output_format = "pdf", output_file = "Diagnose_Repor
 # Step 2: EDA Report
 eda_report(input_data, output_format = "html", output_file = "EDA_Report.html", output_dir = getwd(), browse = FALSE)
 eda_report(input_data, output_format = "pdf", output_file = "EDA_Report.pdf", output_dir = getwd(), browse = FALSE)
+
+# Step 3: dataQualityR-Report
+checkDataQuality(data = input_data, out.file.num = paste(getwd(), "/dq_num.csv", sep = ""), out.file.cat = paste(getwd(), "/dq_cat.csv", sep = ""))
+
+#Step 4: Outlier Diagnosis Plots
+plot_outlier(input_data)
+#Output has to be saved manually
